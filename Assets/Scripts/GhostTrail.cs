3e9ca0365097ed46096c06cccdbb25af0329fe62
Jeremy Gibson Bond <jrgibson@gmail.com>
2025-07-29 18:03:55 +1200

    Made several improvements to the Mix-n-Jam code and art to make it easier for students to remix.

---
+++ Assets/Scripts/GhostTrail.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using DG.Tweening;
+
+public class GhostTrail : MonoBehaviour
+{
+    [SerializeField] private Movement move;
+    [SerializeField] private AnimationScript anim;
+    private SpriteRenderer sr;
+    public Transform ghostsParent;
+    public Color trailColor;
+    public Color fadeColor;
+    public float ghostInterval;
+    public float fadeTime;
+
+    private void Start() {
+        // Replaced the deprecated (de-pre-cated, not "depreciated") FindObjectOfType calls. - JGB 2025-07-29
+        anim ??= FindFirstObjectByType<AnimationScript>(); // FindObjectOfType<AnimationScript>();
+        move ??= FindFirstObjectByType<Movement>(); // FindObjectOfType<Movement>();
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    public void ShowGhost()
+    {
+        Sequence s = DOTween.Sequence();
+
+        for (int i = 0; i < ghostsParent.childCount; i++)
+        {
+            Transform currentGhost = ghostsParent.GetChild(i);
+            s.AppendCallback(()=> currentGhost.position = move.transform.position);
+            s.AppendCallback(() => currentGhost.GetComponent<SpriteRenderer>().flipX = anim.sr.flipX);
+            s.AppendCallback(()=>currentGhost.GetComponent<SpriteRenderer>().sprite = anim.sr.sprite);
+            s.Append(currentGhost.GetComponent<SpriteRenderer>().material.DOColor(trailColor, 0));
+            s.AppendCallback(() => FadeSprite(currentGhost));
+            s.AppendInterval(ghostInterval);
+        }
+    }
+
+    public void FadeSprite(Transform current)
+    {
+        current.GetComponent<SpriteRenderer>().material.DOKill();
+        current.GetComponent<SpriteRenderer>().material.DOColor(fadeColor, fadeTime);
+    }
+
+}
